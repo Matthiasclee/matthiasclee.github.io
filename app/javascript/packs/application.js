1cf8009fa9cf86e64ded88ef12dbd2cb437ef22a
Matthiasclee <matthias@matthiasclee.com>
2022-03-28 11:23:48 -0700

    Added rich text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import "trix"
+import "@rails/actiontext"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
