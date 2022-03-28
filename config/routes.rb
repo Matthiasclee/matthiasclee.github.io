6574573d7d04f0a9d929acb557d39e77f25b9f4a
Matthiasclee <matthias@matthiasclee.com>
2022-03-28 16:00:57 -0700

    Added users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :blog_posts
   get '/blog', to: redirect('/blog_posts')
   get '/blog_posts/:id/embed', to: 'blog_posts#embed', as: 'blog_post_embed'
